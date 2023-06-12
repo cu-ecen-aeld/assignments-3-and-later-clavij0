827c62b3d1ea26fa96734c791e83ba26f3261f12
Jorge Eccel <jorge.clavijo@eccel.co>
2023-06-12 00:54:47 -0500

    Assigments 1 Instructions - added finder.sh and writer.sh

---
+++ finder-app/finder.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#!bin/bash
+
+#Recived 
+
+if [ $# -ne 2 ]; 
+then
+	echo "1.0 error"
+	exit 1
+fi 
+
+
+if [ -d $1 ]
+then
+	var=$(ls $1 | wc -l )
+	#grep -rno "$1" -e "$2"
+	var2=$(grep -rn "$1" -e "$2" | wc -l)
+	if [ $var2 -eq 0 ];
+	then
+		echo "Error 1"
+	else 
+		echo  "The number of files are $var and the number of matching lines are $var2"
+	fi
+	
+else
+	exit 1
+fi
+
+
+
+
+
---
+++ finder-app/writer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; 
+then
+	echo "1.2 error"
+	exit 1
+fi 
+
+if [ -f $1 ];
+then
+    echo $2 > $1
+elif  [ ! -f $1 ]; 
+then
+    mkdir -p "$(dirname "$1")";
+    touch "$1";
+    echo $2 > $1;
+
+else
+    exit 1
+fi
