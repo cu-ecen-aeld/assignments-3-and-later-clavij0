8937160442a4474a179848b4063ca2b08bae40c1
Jorge Clavijo <clavijo.jorge@outlook.com>
2023-12-14 19:08:50 -0500

    edit assignment conf-1

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -7,8 +7,8 @@ set -u
 
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
-WRITEDIR=/tmp/aeld-data
-#WRITEDIR=/tmp/assignment4-result.txt
+#WRITEDIR=/tmp/aeld-data
+WRITEDIR=/tmp/assignment4-result.txt
 #username=$(cat conf/username.txt)
 username=$(whoami)
 FINDER_APP_DIR=$(realpath $(dirname $0))
@@ -39,7 +39,7 @@ rm -rf "${WRITEDIR}"
 # create $WRITEDIR if not assignment1
 # pwd
 #assignment=`cat conf/assignment.txt`
-assignment=$(cat conf/assignment.txt)
+assignment=$(cat conf/assignmsent.txt)
 
 if [ $assignment != 'assignment1' ]
 then
