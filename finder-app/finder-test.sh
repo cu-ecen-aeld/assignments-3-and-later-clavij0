3b8f5c60283341757ca0de6bdfb8770faf71f852
Jorge Clavijo <clavijo.jorge@outlook.com>
2023-12-15 11:47:28 -0500

    conf-4

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -38,6 +38,8 @@ rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
 # pwd
+cat conf/assignment.txt
+echo $(cat conf/assignmsent.txt)
 assignment=`cat conf/assignment.txt`
 cat conf/assignment.txt
 echo $(cat conf/assignmsent.txt)
