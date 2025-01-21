dfcf0623c8810e9f09fe302895a8c95727ad6e93
Jorge Clavijo <clavijo.jorge@outlook.com>
2025-01-21 17:10:11 -0500

    change in write comment *f_pos

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -169,7 +169,7 @@ ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count,
     }
     
     
-    *f_pos +=count;
+    //*f_pos +=count;
     
     retval = count;
     mutex_unlock(&dev->lock);
