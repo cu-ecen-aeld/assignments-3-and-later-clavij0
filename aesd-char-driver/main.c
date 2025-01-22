14b8364a68300292631bf61725229dcd3b072c3c
Jorge Clavijo <clavijo.jorge@outlook.com>
2025-01-21 23:46:50 -0500

    fixing v13

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -167,7 +167,6 @@ ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count,
         //aesd_circular_buffer_add_entry(dev->cir_buff,dev->tmp_entry);
         if (delete_item != NULL){
             //char temp[dev->buffer_entry.size+1];
-            size_t lenght = strlen(delete_item);
 			PDEBUG("Deleted entry %.*s",sizeof(delete_item),delete_item);
             PDEBUG("Data Deleted size: %zu", dev->buffer_entry.size);
 			kfree(delete_item);
