9b1aa1068d4165d8b20253bb80304d5810984f66
Jorge Clavijo <clavijo.jorge@outlook.com>
2025-01-21 22:17:34 -0500

    fixing v7

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -159,6 +159,7 @@ ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count,
     PDEBUG("dev->buffer_entry.size %zu",dev->buffer_entry.size );
 
     if (strchr(dev->buffer_entry.buffptr,'\n') != NULL){
+        dev->buffer_entry.size = '\0';
         PDEBUG("Newline character detected");
         const char *delete_item = aesd_circular_buffer_add_entry(&dev->cir_buff,&dev->buffer_entry);
         PDEBUG("Added entry %s", dev->buffer_entry.buffptr);
@@ -166,6 +167,7 @@ ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count,
         if (delete_item != NULL){
             
 			PDEBUG("Deleted entry %s",sizeof(delete_item),delete_item);
+            PDEBUG("Data Deleted size: %zu", dev->buffer_entry.size);
 			kfree(delete_item);
 		}	
         //once we detect the /n we reset the value of buffptr and size to get the next text.
